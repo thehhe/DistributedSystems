56a3524804ff4805077c1a78252666139350e28d
thehhe <pecakmaciek@gmail.com>
2018-03-23 21:14:12 +0100

    Delete IndividualCustomer.sql

--- Documents/_STUDIA/SEMESTR3/PecakProjektKonferencje/VIEWS/IndividualCustomer.sql
+++
@@ -1,5 +0,0 @@
-CREATE VIEW dbo.IndividualCustomers
-	AS
-	SELECT Name, Surname, Phone, LoginName, Email, Street, City, PostalCode, Country
-	FROM dbo.Customers
-	WHERE IsCompany IS NULL OR IsCompany=0
