dd2e7ef34462cdc25915c0446698022add917eee
thehhe <pecakmaciek@gmail.com>
2018-03-23 21:13:50 +0100

    Delete ShowCancelledWshopReservations.sql

--- Documents/_STUDIA/SEMESTR3/PecakProjektKonferencje/VIEWS/ShowCancelledWshopReservations.sql
+++
@@ -1,5 +0,0 @@
-CREATE VIEW dbo.ShowCancelledWshopReservations
-AS
-SELECT WorkshopSignUpID, WorkshopID, ConferenceSignUpID, NumberOfPpl, NumberOfStudents, Cancelled
-FROM dbo.WorkshopSignUps
-WHERE (Cancelled=1)
