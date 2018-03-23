8e5d2eccec7f2d8707250de0d433ad9d89f1e725
thehhe <pecakmaciek@gmail.com>
2018-03-23 21:14:27 +0100

    Delete ShowCancelledConferenceReservations.sql

--- Documents/_STUDIA/SEMESTR3/PecakProjektKonferencje/VIEWS/ShowCancelledConferenceReservations.sql
+++
@@ -1,5 +0,0 @@
-CREATE VIEW dbo.ShowCancelledConferenceReservations
-	AS
-	SELECT ConferenceSignUpID, ConferenceDayID, CustomerID, SignUpDate, Price, NumberOfPpl, NumberOfStudents, Cancelled
-	FROM dbo.ConferenceSignUps
-	WHERE (Cancelled=1)
