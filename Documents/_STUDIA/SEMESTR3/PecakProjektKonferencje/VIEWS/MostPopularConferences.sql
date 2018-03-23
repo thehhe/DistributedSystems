87a19bbd4db71f59c0504c7dd564b3723ab188f6
thehhe <pecakmaciek@gmail.com>
2018-03-23 21:14:07 +0100

    Delete MostPopularConferences.sql

--- Documents/_STUDIA/SEMESTR3/PecakProjektKonferencje/VIEWS/MostPopularConferences.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIEW dbo.MostPopularConferences
-	AS
-	SELECT TOP 5 C.ConferenceID, C.Name, SUM(SU.NumberOfPpl) as PotentialAttenders
-	FROM dbo.Conferences as C
-		INNER JOIN dbo.ConferenceDays as CD on C.ConferenceID=CD.ConferenceID
-		INNER JOIN dbo.ConferenceSignUps as SU on CD.ConferenceDayID=SU.ConferenceDayID
-	WHERE SU.Cancelled IS NULL OR SU.Cancelled=0
-	GROUP BY C.ConferenceID, C.Name
-	ORDER BY PotentialAttenders DESC
