846a1d93736650ddf82126f3e3a29c6affcd4fac
thehhe <pecakmaciek@gmail.com>
2018-03-23 21:14:02 +0100

    Delete MostPopularWorkshops.sql

--- Documents/_STUDIA/SEMESTR3/PecakProjektKonferencje/VIEWS/MostPopularWorkshops.sql
+++
@@ -1,8 +0,0 @@
-CREATE VIEW dbo.MostPopularWorkshops
-	AS
-	SELECT TOP 5 dbo.Workshops.WorkshopID, dbo.Workshops.Name, SUM(dbo.WorkshopSignUps.NumberOfPpl) AS Attenders
-	FROM dbo.WorkshopSignUps INNER JOIN
-		 dbo.Workshops ON dbo.WorkshopSignUps.WorkshopID = dbo.Workshops.WorkshopID
-	WHERE (dbo.WorkshopSignUps.Cancelled IS NULL OR dbo.WorkshopSignUps.Cancelled=0)
-	GROUP BY dbo.Workshops.WorkshopID, dbo.Workshops.Name
-	ORDER BY Attenders DESC
